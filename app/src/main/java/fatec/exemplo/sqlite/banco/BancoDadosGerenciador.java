08a9987f5c65693e94a63bc47cfa9001a235a539
larissa <larissa.c.hirata@gmail.com>
2019-12-03 09:14:14 -0300

    comiit de codigo do banco

--- app/src/main/java/fatec/exemplo/sqlite/banco/BancoDadosGerenciador.java
+++ app/src/main/java/fatec/exemplo/sqlite/banco/BancoDadosGerenciador.java
@@ -6,6 +6,8 @@ import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 
+import java.util.ArrayList;
+
 public class BancoDadosGerenciador {
 
     private BancoDeDados dbHelper;
@@ -70,4 +72,74 @@ public class BancoDadosGerenciador {
         database.delete(BancoDeDados.TABELA, BancoDeDados.ID + "=" + _id, null);
     }
 
+    public String novoProduto(String nomep, String preco) {
+
+        long resultado;
+        ContentValues valores = new ContentValues();
+        valores.put(BancoDeDados.NOMEP, nomep);
+        valores.put(BancoDeDados.PRECO, preco);
+
+        resultado = database.insert(BancoDeDados.PRODUTO, null, valores);
+
+        if (resultado == -1) {
+            return "Erro ao inserir registro.";
+        } else {
+            return "Registro inserido com sucesso !";
+        }
+    }
+
+
+    public Cursor listaProdutos() {
+        String[] columns = new String[] { BancoDeDados.IDP, BancoDeDados.NOMEP, BancoDeDados.PRECO };
+        Cursor cursor = database.query(BancoDeDados.PRODUTO, columns, null,
+                null, null, null, null);
+        if (cursor != null) {
+            cursor.moveToFirst();
+        }
+        return cursor;
+    }
+
+    public String comprarProduto(String idProduto, String idCliente){
+
+
+        // Pegar nome produto
+
+        // Pegar nome cliente
+
+        long resultado;
+        ContentValues valores = new ContentValues();
+        valores.put(BancoDeDados.IDCLINTE, idCliente);
+        valores.put(BancoDeDados.IDPRODUTO, idProduto);
+
+        resultado = database.insert(BancoDeDados.TABELA, null, valores);
+
+        if (resultado == -1) {
+            return "Erro ao finalizar compra.";
+        } else {
+            return "Compra realizada com sucesso !";
+        }
+    }
+
+    public Cursor historicoCompra(){
+        String historico[][] = new String[10][2];
+        ArrayList<String> _cliente = new ArrayList<String>();
+        ArrayList<String> _produto = new ArrayList<String>();
+
+
+        Cursor cliente = this.fetch(); // cliente
+        Cursor produto = this.listaProdutos(); // produtos
+
+        String query = "select cliente.id, cliente.nome, produto.idp, produto.nomep from cliente, produto where cliente.id and produto.id";
+        database.rawQuery(query, new String[] {});
+
+        String[] columns = new String[] { BancoDeDados.ID, BancoDeDados.NOME, BancoDeDados.ENDERECO,
+                BancoDeDados.IDADE };
+        Cursor cursor = database.query(BancoDeDados.PRODUTO_CLIENTE,  columns, null,
+                null, null, null, null);
+        if (cursor != null) {
+            cursor.moveToFirst();
+        }
+        return cursor;
+
+    }
 }
--- app/src/main/java/fatec/exemplo/sqlite/banco/BancoDeDados.java
+++ app/src/main/java/fatec/exemplo/sqlite/banco/BancoDeDados.java
@@ -15,6 +15,23 @@ public class BancoDeDados extends SQLiteOpenHelper {
     public static final String ENDERECO = "endereco";
     public static final String IDADE = "idade";
 
+
+    public static final String PRODUTO = "produto";
+
+    // Table columns
+    public static final String IDP = "_id";
+    public static final String NOMEP = "nome";
+    public static final String PRECO = "preco";
+
+    public static final String PRODUTO_CLIENTE  = "cliente";
+
+    // Table columns
+    public static final String IDCOMPRA = "_id";
+    public static final String IDCLINTE = "id_compra";
+    public static final String IDPRODUTO = "id_produto";
+
+
+
     // Database Information
     static final String DB_NAME = "exemplo_cliente.db";
 
@@ -34,13 +51,28 @@ public class BancoDeDados extends SQLiteOpenHelper {
                 NOME + " TEXT, " +
                 ENDERECO + " TEXT, " +
                 IDADE + " TEXT ) ";
-
         db.execSQL(sql);
+
+
+        String _sql = "CREATE TABLE "+PRODUTO +
+                "( " + ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                NOMEP + " TEXT, " +
+                PRECO + " TEXT ) ";
+        db.execSQL(_sql);
+
+        String sql_ = "CREATE TABLE "+PRODUTO_CLIENTE +
+                "( " + IDCOMPRA + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                 IDCLINTE + " TEXT, " +
+                IDPRODUTO + " TEXT ) " +
+                "";
+        db.execSQL(sql_);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + TABELA);
+        db.execSQL("DROP TABLE IF EXISTS " + PRODUTO);
+        db.execSQL("DROP TABLE IF EXISTS " + PRODUTO_CLIENTE );
         onCreate(db);
     }
 }
